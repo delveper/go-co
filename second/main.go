04bbb7ce16b8217bf9fdadcc3cfc6f859b7261b3
Eugene <yevhen@bilyk.dev>
2022-07-24 10:33:01 +0300

    solved

--- second/main.go
+++ second/main.go
@@ -1,17 +1,35 @@
 package main
 
-// Конкурентно порахувати суму усіх слайсів int, та роздрукувати результат.
-// Приклад:
-// [ [ 4, 6 ], [ 7, 9 ] ]
-// Результат друку:
-// “result: 26”
-func main() {
-	// Розкоментуй мене)
-	// n := [][]int{
-	// 	{2, 6, 9, 24},
-	// 	{7, 3, 94, 3, 0},
-	// 	{4, 2, 8, 35},
-	// }
+import (
+	"fmt"
+)
 
-	// Ваша реалізація
+func main() {
+	n := [][]int{
+		{2, 6, 9, 24},
+		{7, 3, 94, 3, 0},
+		{4, 2, 8, 35},
+	}
+	slices := make(chan []int)
+	var result int
+
+	go split(slices, n)
+	for s := range slices {
+		result += sum(s)
+	}
+	fmt.Printf("result: %v\n", result)
+}
+func split(slices chan []int, n [][]int) {
+	for _, s := range n {
+		slices <- s
+	}
+	close(slices)
+}
+
+func sum(s []int) int {
+	var E int
+	for _, v := range s {
+		E += v
+	}
+	return E
 }
